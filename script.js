f50e24c71006328500a81afa459712f08c1a389c
Andrea Iannì <andrea.ianni@avbo.it>
2023-10-26 15:36:17 +0200

    cosmetics: added some comments

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+//Aggiunge un evento che si attiva quando il DOM viene caricato
+//	e richiama la funzione onAllLoaded
 document.addEventListener("DOMContentLoaded", onAllLoaded);
 
+//Definizione di una funzione che prende in input un argomento
 function onAllLoaded(event) {
+	//Stampa in console un messaggio
 	console.log("DOM loaded");
 }
