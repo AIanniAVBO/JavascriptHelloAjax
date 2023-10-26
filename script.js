3dff19617e48cd5cc1ff1ebf30536d316636c6f7
Andrea Iannì <andrea.ianni@avbo.it>
2023-10-26 15:42:28 +0200

    dev: added a var table that contains wordsTable element, searched all td inside it and print them to the console

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 //Aggiunge un evento che si attiva quando il DOM viene caricato
 //	e richiama la funzione onAllLoaded
 document.addEventListener("DOMContentLoaded", onAllLoaded);
-
+//Crea una variabile globale, quindi visibile in tutto il codice, di nome table
+var table;
 //Definizione di una funzione che prende in input un argomento
 function onAllLoaded(event) {
 	//Stampa in console un messaggio
 	console.log("DOM loaded");
+	
+	//Ricerca la table all'interno del DOM e la salva nella variabile creata in precedenza
+	table = document.getElementById("wordsTable");
+	//Ottiene un array con dentro tutti i figli di table aventi il come tag td
+	let allTDs = table.getElementsByTagName('td');
+	//Stampa tutti i td
+	console.log(allTDs);
 }
